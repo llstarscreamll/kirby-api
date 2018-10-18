87bf63fb8e62ec205e050fae4631789ad4e5b0f3
Johan Alvarez <llstarscreamll@hotmail.com>
2018-10-17 22:15:36 -0500

    delete unused files on authentication package

--- packages/llstarscreamll/Authentication/rewriteRules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Available placeholders: llstarscreamll, Authentication, llstarscreamll, authentication
-return [
-    'src/MyPackage.php' => 'src/Authentication.php',
-    'config/mypackage.php' => 'config/authentication.php',
-    'src/Facades/MyPackage.php' => 'src/Facades/Authentication.php',
-    'src/MyPackageServiceProvider.php' => 'src/AuthenticationServiceProvider.php',
-];
