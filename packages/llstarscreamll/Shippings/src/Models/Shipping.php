f25b051ed725c6b5eb65dd1252e42e468b937f49
Johan Alvarez <llstarscreamll@hotmail.com>
2018-10-21 21:34:09 -0500

    set fillable and dates attribute to Shipping model and SoftDeletes trait

--- packages/llstarscreamll/Shippings/src/Models/Shipping.php
+++ packages/llstarscreamll/Shippings/src/Models/Shipping.php
@@ -2,8 +2,32 @@
 namespace llstarscreamll\Shippings\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Shipping extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public $fillable = [
+        'name',
+        'address',
+        'phone',
+        'delivery_instructions',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
