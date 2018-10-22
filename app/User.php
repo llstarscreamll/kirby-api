3e2395c210b764b0864df05cf62b5fbba17aae00
Johan Alvarez <llstarscreamll@hotmail.com>
2018-10-21 19:17:36 -0500

    formatting code

--- app/User.php
+++ app/User.php
@@ -1,10 +1,8 @@
 <?php
-
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
