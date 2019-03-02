64a437a4f8c515d2b7af13dd130552a7828946af
Johan Alvarez <llstarscreamll@hotmail.com>
2019-03-02 16:15:39 -0500

    Refactor codeception Api helper on authentication package

--- packages/llstarscreamll/Authentication/tests/_support/Helper/Api.php
+++ packages/llstarscreamll/Authentication/tests/_support/Helper/Api.php
@@ -3,17 +3,21 @@ namespace Authentication\Helper;
 
 use llstarscreamll\Users\Models\User;
 
-// here you can define custom actions
-// all public methods declared in helper class will be available in $I
-
+/**
+ * Class Api.
+ *
+ * @author Johan Alvarez <llstarscreamll@hotmail.com>
+ */
 class Api extends \Codeception\Module
 {
     /**
      * Create and log in the admin user.
      *
+     * @param  \llstarscreamll\Users\Models\User|null $user
+     * @param  string                                 $driver
      * @return App\Containers\User\Models\User
      */
-    public function amLoggedAsUser($user = null, string $driver = 'api')
+    public function amLoggedAsUser(User $user = null, string $driver = 'api'): User
     {
         if (is_array($user)) {
             $user = User::create($user);
@@ -21,7 +25,7 @@ class Api extends \Codeception\Module
 
         if (is_null($user)) {
             $user = User::create([
-                'name'     => 'admin',
+                'name'     => 'admin user',
                 'email'    => 'admin@admin.com',
                 'password' => bcrypt('admin'),
             ]);
@@ -31,12 +35,13 @@ class Api extends \Codeception\Module
     }
 
     /**
-     * Log in the given user.
+     * Log in the given user on the given guard.
      *
      * @param  \llstarscreamll\Users\Models\User   $user
+     * @param  string                              $driver
      * @return \llstarscreamll\Users\Models\User
      */
-    public function loginUser(User $user, string $driver = 'api')
+    public function loginUser(User $user, string $driver = 'api'): User
     {
         app('auth')->guard($driver)->setUser($user);
         app('auth')->shouldUse($driver);
