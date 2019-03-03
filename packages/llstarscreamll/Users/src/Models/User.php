85d2f497ffef82e2e882c659833dc78c44805c63
Johan Alvarez <llstarscreamll@hotmail.com>
2019-03-03 12:04:00 -0500

    Add spatie HasRoles trait to User model

--- packages/llstarscreamll/Users/src/Models/User.php
+++ packages/llstarscreamll/Users/src/Models/User.php
@@ -5,10 +5,16 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
+/**
+ * Class User.
+ *
+ * @author Johan Alvarez <llstarscreamll@hotmail.com>
+ */
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable, SoftDeletes;
+    use HasRoles, HasApiTokens, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
