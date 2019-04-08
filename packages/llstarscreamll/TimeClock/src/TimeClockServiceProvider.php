07ab4b871bf524815635a92aef126eb48aa3f7ff
Johan Alvarez <llstarscreamll@hotmail.com>
2019-04-07 21:59:32 -0500

    refactor(TimeClock): rename config strings to slug case conventions

--- packages/llstarscreamll/TimeClock/src/TimeClockServiceProvider.php
+++ packages/llstarscreamll/TimeClock/src/TimeClockServiceProvider.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace llstarscreamll\TimeClock;
 
 use Illuminate\Support\ServiceProvider;
@@ -15,7 +14,7 @@ class TimeClockServiceProvider extends ServiceProvider
     {
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'llstarscreamll');
         // $this->loadViewsFrom(__DIR__.'/../resources/views', 'llstarscreamll');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/Data/Migrations');
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         // Publishing is only necessary when using the CLI.
@@ -31,11 +30,11 @@ class TimeClockServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/timeclock.php', 'timeclock');
+        $this->mergeConfigFrom(__DIR__.'/../config/time-clock.php', 'time-clock');
 
         // Register the service the package provides.
-        $this->app->singleton('timeclock', function ($app) {
-            return new TimeClock;
+        $this->app->singleton('TimeClock', function ($app) {
+            return new TimeClock();
         });
     }
 
@@ -46,9 +45,9 @@ class TimeClockServiceProvider extends ServiceProvider
      */
     public function provides()
     {
-        return ['timeclock'];
+        return ['TimeClock'];
     }
-    
+
     /**
      * Console-specific booting.
      *
@@ -58,23 +57,23 @@ class TimeClockServiceProvider extends ServiceProvider
     {
         // Publishing the configuration file.
         $this->publishes([
-            __DIR__.'/../config/timeclock.php' => config_path('timeclock.php'),
-        ], 'timeclock.config');
+            __DIR__.'/../config/time-clock.php' => config_path('time-clock.php'),
+        ], 'time-clock.config');
 
         // Publishing the views.
         /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/llstarscreamll'),
-        ], 'timeclock.views');*/
+        __DIR__.'/../resources/views' => base_path('resources/views/vendor/llstarscreamll'),
+        ], 'time-clock.views');*/
 
         // Publishing assets.
         /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/llstarscreamll'),
-        ], 'timeclock.views');*/
+        __DIR__.'/../resources/assets' => public_path('vendor/llstarscreamll'),
+        ], 'time-clock.views');*/
 
         // Publishing the translation files.
         /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/llstarscreamll'),
-        ], 'timeclock.views');*/
+        __DIR__.'/../resources/lang' => resource_path('lang/vendor/llstarscreamll'),
+        ], 'time-clock.views');*/
 
         // Registering package commands.
         // $this->commands([]);
