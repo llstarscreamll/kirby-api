5d6a0dddf7476e959e2068cbaa93e33e89ad987d
Johan Alvarez <llstarscreamll@hotmail.com>
2019-04-28 16:45:39 -0500

    build: simplify fix permissions bash script

--- fixPermissions.sh
+++ fixPermissions.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-sudo chown -R www-data:www-data .
 sudo usermod -a -G www-data $USER
-sudo chown -R $USER:www-data ./
-sudo find ./ -type f -exec chmod 644 {} \;
-sudo find ./ -type d -exec chmod 755 {} \;
+sudo chown -R $USER:www-data .
 sudo chgrp -R www-data storage bootstrap/cache
 sudo chmod -R ug+rwx storage bootstrap/cache
