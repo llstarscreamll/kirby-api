c356eac1f24467afba0f0444bf8eb813803cfc4b
Johan Alvarez <llstarscreamll@hotmail.com>
2019-05-07 22:28:37 -0500

    feat(Employees): create identification entity factory

---
+++ packages/llstarscreamll/Employees/database/factories/IdentificationFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+use Faker\Generator as Faker;
+use llstarscreamll\Employees\Models\Employee;
+use llstarscreamll\Employees\Models\Identification;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+ */
+
+$factory->define(Identification::class, function (Faker $faker) {
+    return [
+        'employee_id' => factory(Employee::class)->create()->id,
+        'name' => $faker->randomElement(['E-card', 'PIN']),
+        'code' => "{$faker->word}-{$faker->randomNumber($faker->numberBetween(4, 8))}",
+    ];
+});
