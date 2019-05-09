9eecf475d7786062ab31f1356ed92b0552ca8cec
Johan Alvarez <llstarscreamll@hotmail.com>
2019-05-09 00:45:50 -0500

    fix(Company): fix cost center repository class name

--- packages/llstarscreamll/Company/src/Data/Repositories/EloquentCostCenterRepository.php
+++ packages/llstarscreamll/Company/src/Data/Repositories/EloquentCostCenterRepository.php
@@ -11,7 +11,7 @@ use llstarscreamll\Company\Contracts\CostCenterRepositoryInterface;
  *
  * @author Johan Alvarez <llstarscreamll@hotmail.com>
  */
-class EloquentEmployeeRepository extends EloquentRepositoryAbstract implements CostCenterRepositoryInterface
+class EloquentCostCenterRepository extends EloquentRepositoryAbstract implements CostCenterRepositoryInterface
 {
     /**
      * @var array
