8297b649bd0b9548ae77c6af40ba2642812e146b
Johan Alvarez <jalvarez@merqueo.com>
2019-05-19 01:10:51 -0500

    fix(WorkShifts): fix error refreshing migrations by dropping child table before the parent one

--- packages/llstarscreamll/WorkShifts/src/Data/Migrations/2019_02_27_224848_create_work_shifts_tables.php
+++ packages/llstarscreamll/WorkShifts/src/Data/Migrations/2019_02_27_224848_create_work_shifts_tables.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 /**
  * Class CreateWorkShiftsTables.
@@ -47,6 +47,7 @@ class CreateWorkShiftsTables extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('employee_work_shift');
         Schema::dropIfExists('work_shifts');
     }
 }
