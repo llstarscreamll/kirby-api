715b8e9df3f23346c9d088323a196e379667d1e7
Johan Alvarez <llstarscreamll@hotmail.com>
2019-06-04 00:13:53 -0500

    buld: make fixPermissions.sh command less invasive

--- fixPermissions.sh
+++ fixPermissions.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 sudo usermod -a -G www-data $USER
-sudo chown -R $USER:www-data .
-sudo chgrp -R www-data storage bootstrap/cache
-sudo chmod -R ug+rwx storage bootstrap/cache
+sudo chgrp -R www-data storage/* bootstrap/cache/*
+sudo chmod -R ug+rwx storage/* bootstrap/cache/*
