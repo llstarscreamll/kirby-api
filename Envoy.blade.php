8e284d43ef44d67dcdee16a9d14959ec6b6e6c29
Johan Alvarez <llstarscreamll@hotmail.com>
2019-06-09 14:47:59 -0500

    build: update how to instanciate Dotenv class on Envoy file

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,6 @@
 @setup
 require __DIR__.'/vendor/autoload.php';
-(new \Dotenv\Dotenv(__DIR__, '.env'))->load();
+\Dotenv\Dotenv::create(__DIR__, '.env')->load();
 
 $site = env('SITE');
 $userAndServer = explode(';', env(strtoupper($target ?? 'lab').'_SERVERS'));
