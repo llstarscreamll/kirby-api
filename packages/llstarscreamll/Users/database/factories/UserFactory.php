6aed15b3fd1f53bb3160294cf709c74488f7c952
Johan Alvarez <llstarscreamll@hotmail.com>
2019-06-17 23:51:21 -0500

    refactor: remove default user factory and improve the User package one

--- packages/llstarscreamll/Users/database/factories/UserFactory.php
+++ packages/llstarscreamll/Users/database/factories/UserFactory.php
@@ -17,7 +17,8 @@ $factory->define(llstarscreamll\Users\Models\User::class, function (Faker $faker
     return [
         'first_name' => $faker->firstName,
         'last_name' => $faker->lastName,
-        'email' => $faker->email,
-        'password' => bcrypt('123'),
+        'email_verified_at' => now(),
+        'email' => $faker->unique()->safeEmail,
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
     ];
 });
