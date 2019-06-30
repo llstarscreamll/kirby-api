9719b238d5a543929af7932f3ff60b7f52168aa2
Johan Alvarez <llstarscreamll@hotmail.com>
2019-06-29 23:38:14 -0500

    feat(build): change chgrp to chown no fixpermissions.sh

--- fixPermissions.sh
+++ fixPermissions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo usermod -a -G www-data $USER
-sudo chgrp -R www-data storage/* bootstrap/cache/*
+sudo chown -R $USER:www-data storage/* bootstrap/cache/*
 sudo chmod -R ug+rwx storage/* bootstrap/cache/*
