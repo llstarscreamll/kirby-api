e6485707d137f6d0a409f280d9d508afb635faeb
Johan Alvarez <llstarscreamll@hotmail.com>
2019-07-25 21:19:52 -0500

    feat(TimeClock): create and configure time package seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             AuthorizationPackageSeeder::class,
             WorkShiftsPackageSeeder::class,
             DefaultNoveltyTypesSeed::class,
+            TimeClockPackageSeeder::class,
         ]);
     }
 }
---
+++ packages/llstarscreamll/TimeClock/database/seeds/TimeClockPackageSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+/**
+ * Class TimeClockPackageSeeder.
+ *
+ * @author Johan Alvarez <llstarscreamll@hotmail.com>
+ */
+class TimeClockPackageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            TimeClockPermissionsSeeder::class,
+        ]);
+    }
+}
