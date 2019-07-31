1b37d09bc03fefd42333d24e9e79817089cbb739
Johan Alvarez <llstarscreamll@hotmail.com>
2019-07-30 22:44:19 -0500

    feat(Users): return first and last name on current user API endpoint

--- packages/llstarscreamll/Users/src/UI/API/Resources/UserResource.php
+++ packages/llstarscreamll/Users/src/UI/API/Resources/UserResource.php
@@ -22,13 +22,15 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'          => $this->id,
-            'name'        => $this->name,
-            'email'       => $this->email,
-            'roles'       => RoleResource::collection($this->whenLoaded('roles')),
+            'id' => $this->id,
+            'name' => $this->name,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'roles' => RoleResource::collection($this->whenLoaded('roles')),
             'permissions' => PermissionResource::collection($this->whenLoaded('permissions')),
-            'created_at'  => $this->created_at,
-            'updated_at'  => $this->updated_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
         ];
     }
 }
