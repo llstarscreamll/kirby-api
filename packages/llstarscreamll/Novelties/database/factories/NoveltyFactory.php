cfdbbed3e6b12a67612884c87dfdc490a94384f0
Johan Alvarez <llstarscreamll@hotmail.com>
2019-08-08 22:15:06 -0500

    feat(Novelties): create novelty factory

---
+++ packages/llstarscreamll/Novelties/database/factories/NoveltyFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+use Faker\Generator as Faker;
+use llstarscreamll\Novelties\Models\NoveltyType;
+use llstarscreamll\Novelties\Enums\NoveltyTypeOperator;
+use llstarscreamll\Novelties\Models\Novelty;
+use llstarscreamll\Employees\Models\Employee;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+ */
+
+$factory->define(Novelty::class, function (Faker $faker) {
+    $randomCode = $faker->bothify('##??');
+
+    return [
+        'time_clock_log_id' => null,
+        'employee_id' => factory(Employee::class)->create()->id,
+        'novelty_type_id' => factory(NoveltyType::class)->create()->id,
+        'total_time_in_minutes' => $faker->numberBetween(100, 1000),
+    ];
+});
