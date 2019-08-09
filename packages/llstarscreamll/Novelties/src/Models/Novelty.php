74bb8a20ba97707d8027bfcc4882e0a46ce5a6ce
Johan Alvarez <llstarscreamll@hotmail.com>
2019-08-08 22:14:21 -0500

    feat(Novelties): new relations added to Novelty model

--- packages/llstarscreamll/Novelties/src/Models/Novelty.php
+++ packages/llstarscreamll/Novelties/src/Models/Novelty.php
@@ -4,6 +4,8 @@ namespace llstarscreamll\Novelties\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use llstarscreamll\Employees\Models\Employee;
+use llstarscreamll\TimeClock\Models\TimeClockLog;
 
 /**
  * Class Novelty.
@@ -43,5 +45,21 @@ class Novelty extends Model
     public function noveltyType()
     {
         return $this->belongsTo(NoveltyType::class);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function timeClockLog()
+    {
+        return $this->belongsTo(TimeClockLog::class);
     }
 }
