b6c4649c54a1d5c1f24776326b76ec8d3b6128ec
Johan Alvarez <llstarscreamll@hotmail.com>
2019-08-10 06:06:19 +0000

    Apply fixes from StyleCI

--- packages/llstarscreamll/Novelties/src/UI/API/V1/Controllers/NoveltiesController.php
+++ packages/llstarscreamll/Novelties/src/UI/API/V1/Controllers/NoveltiesController.php
@@ -12,70 +12,77 @@ use llstarscreamll\Novelties\UI\API\V1\Requests\GetNoveltyRequest;
  *
  * @author Johan Alvarez <llstarscreamll@hotmail.com>
  */
-class NoveltiesController {
-	/**
-	 * @var \llstarscreamll\Novelties\Contracts\NoveltyRepositoryInterface
-	 */
-	private $noveltyRepository;
+class NoveltiesController
+{
+    /**
+     * @var \llstarscreamll\Novelties\Contracts\NoveltyRepositoryInterface
+     */
+    private $noveltyRepository;
 
-	/**
-	 * @param NoveltyRepositoryInterface $noveltyRepository
-	 */
-	public function __construct(NoveltyRepositoryInterface $noveltyRepository) {
-		$this->noveltyRepository = $noveltyRepository;
-	}
+    /**
+     * @param NoveltyRepositoryInterface $noveltyRepository
+     */
+    public function __construct(NoveltyRepositoryInterface $noveltyRepository)
+    {
+        $this->noveltyRepository = $noveltyRepository;
+    }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index() {
-		//
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request    $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request) {
-		//
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request    $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int                         $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show($id, GetNoveltyRequest $request) {
-		$novelty = $this->noveltyRepository
-			->with(['noveltyType', 'employee.user', 'timeClockLog'])
-			->find($id);
+    /**
+     * Display the specified resource.
+     *
+     * @param  int                         $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id, GetNoveltyRequest $request)
+    {
+        $novelty = $this->noveltyRepository
+            ->with(['noveltyType', 'employee.user', 'timeClockLog'])
+            ->find($id);
 
-		return NoveltyResource::make($novelty);
-	}
+        return NoveltyResource::make($novelty);
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request    $request
-	 * @param  int                         $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request, $id) {
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request    $request
+     * @param  int                         $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int                         $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy($id) {
-		//
-	}
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int                         $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
