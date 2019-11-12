cf5689eab1999e91355d6019970fed0d596f0b37
Johan Alvarez <llstarscreamll@hotmail.com>
2019-11-12 00:03:01 -0500

    feat(company): create cost center Eloquent resource

---
+++ packages/kirby/company/src/UI/API/Resources/CostCenterResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Kirby\Company\UI\API\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class CostCenterResource.
+ *
+ * @author Johan Alvarez <llstarscreamll@hotmail.com>
+ */
+class CostCenterResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
