00d9976f3eb875ac84b7338218d756aa4ad3f0bb
Johan Alvarez <llstarscreamll@hotmail.com>
2020-03-01 14:49:37 +0000

    Apply fixes from StyleCI

--- packages/kirby/time-clock/tests/_support/UnitTester.php
+++ packages/kirby/time-clock/tests/_support/UnitTester.php
@@ -1,8 +1,9 @@
 <?php
+
 namespace ClockTime;
 
 /**
- * Inherited Methods
+ * Inherited Methods.
  * @method void wantToTest($text)
  * @method void wantTo($text)
  * @method void execute($callable)
@@ -15,12 +16,12 @@ namespace ClockTime;
  * @method void pause()
  *
  * @SuppressWarnings(PHPMD)
-*/
+ */
 class UnitTester extends \Codeception\Actor
 {
     use _generated\UnitTesterActions;
 
-   /**
-    * Define custom actions here
-    */
+    /**
+     * Define custom actions here.
+     */
 }
