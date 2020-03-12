f7008b2d878e51fba82f0db1739d1305d4af8140
Johan Alvarez <llstarscreamll@hotmail.com>
2020-03-12 04:07:49 +0000

    Apply fixes from StyleCI

--- packages/kirby/novelties/src/Actions/RegisterTimeClockNoveltiesAction.php
+++ packages/kirby/novelties/src/Actions/RegisterTimeClockNoveltiesAction.php
@@ -3,18 +3,18 @@
 namespace Kirby\Novelties\Actions;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
-use Kirby\Novelties\Enums\DayType;
-use Kirby\Novelties\Models\Novelty;
-use Kirby\Novelties\Models\NoveltyType;
-use Kirby\TimeClock\Models\TimeClockLog;
-use Kirby\Novelties\Enums\NoveltyTypeOperator;
 use Kirby\Company\Contracts\HolidayRepositoryInterface;
 use Kirby\Novelties\Contracts\NoveltyRepositoryInterface;
 use Kirby\Novelties\Contracts\NoveltyTypeRepositoryInterface;
+use Kirby\Novelties\Enums\DayType;
+use Kirby\Novelties\Enums\NoveltyTypeOperator;
+use Kirby\Novelties\Models\Novelty;
+use Kirby\Novelties\Models\NoveltyType;
 use Kirby\TimeClock\Contracts\TimeClockLogRepositoryInterface;
-use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Kirby\TimeClock\Models\TimeClockLog;
 
 /**
  * Class RegisterTimeClockNoveltiesAction.
