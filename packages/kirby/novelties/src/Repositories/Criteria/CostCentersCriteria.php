7a57a5335714b9cf3a8d22b2caf824dd61d14588
Johan Alvarez <llstarscreamll@hotmail.com>
2020-06-06 10:36:46 -0500

    feat(novelties): novelties can be searched by related cost centers

---
+++ packages/kirby/novelties/src/Repositories/Criteria/CostCentersCriteria.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Kirby\Novelties\Repositories\Criteria;
+
+use Prettus\Repository\Contracts\CriteriaInterface;
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Class CostCentersCriteria.
+ */
+class CostCentersCriteria implements CriteriaInterface
+{
+    /**
+     * @var int[]
+     */
+    private $costCenterIds;
+
+    /**
+     * @param int[] $costCenterIds
+     */
+    public function __construct(array $costCenterIds)
+    {
+        $this->costCenterIds = $costCenterIds;
+    }
+
+    /**
+     * Apply criteria in query repository.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @param  RepositoryInterface                 $repository
+     * @return mixed
+     */
+    public function apply($model, RepositoryInterface $repository)
+    {
+        return $model
+            ->join('sub_cost_centers', 'sub_cost_centers.id', 'novelties.sub_cost_center_id')
+            ->whereIn('sub_cost_centers.cost_center_id', $this->costCenterIds);
+    }
+}
