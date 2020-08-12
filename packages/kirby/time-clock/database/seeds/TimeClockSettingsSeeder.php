d3970e58d1a53891eb95096c494ce76301b446f4
Johan Alvarez <llstarscreamll@users.noreply.github.com>
2020-08-12 01:46:57 +0000

    Apply fixes from StyleCI

--- packages/kirby/time-clock/database/seeds/TimeClockSettingsSeeder.php
+++ packages/kirby/time-clock/database/seeds/TimeClockSettingsSeeder.php
@@ -19,13 +19,13 @@ class TimeClockSettingsSeeder extends Seeder
             'key' => 'time-clock.require-novelty-type-for-non-punctual-checks',
             'name' => 'Require novelty type for non punctual checks',
             'description' => 'If true, then novelty type is required for non punctual checkin or checkout',
-            'value' => "0",
+            'value' => '0',
         ],
         [
             'key' => 'time-clock.adjust-scheduled-novelty-datetime-based-on-checks',
             'name' => 'Adjust scheduled novelties datetime based on checks',
             'description' => 'If true, then scheduled novelties start/end datetime are adjusted relative to check in/out datetime',
-            'value' => "1",
+            'value' => '1',
         ],
     ];
 
