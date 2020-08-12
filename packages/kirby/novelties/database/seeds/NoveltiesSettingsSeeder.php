19c811c9987bd2b1e8b1cdb7a85e73ba77cc57dd
Johan Alvarez <llstarscreamll@hotmail.com>
2020-08-11 20:43:38 -0500

    fix(novelties): error seeding settings with datatype

--- packages/kirby/novelties/database/seeds/NoveltiesSettingsSeeder.php
+++ packages/kirby/novelties/database/seeds/NoveltiesSettingsSeeder.php
@@ -35,28 +35,28 @@ class NoveltiesSettingsSeeder extends Seeder
                 'name' => 'Default addition novelty type',
                 'description' => 'Default novelty type for addition operations',
                 'data_type' => 'int',
-                'value' => NoveltyType::where('code', 'HADI')->first()->id,
+                'value' => (string) NoveltyType::where('code', 'HADI')->first()->id,
             ],
             [
                 'key' => 'novelties.default-subtraction-novelty-type',
                 'name' => 'Default subtraction novelty type',
                 'description' => 'Default novelty type for subtraction operations',
                 'data_type' => 'int',
-                'value' => NoveltyType::where('code', 'PP')->first()->id,
+                'value' => (string) NoveltyType::where('code', 'PP')->first()->id,
             ],
             [
                 'key' => 'novelties.default-addition-balance-novelty-type',
                 'name' => 'Default addition balance novelty type ',
                 'description' => 'Default novelty type for addition balance',
                 'data_type' => 'int',
-                'value' => NoveltyType::where('code', 'B+')->first()->id,
+                'value' => (string) NoveltyType::where('code', 'B+')->first()->id,
             ],
             [
                 'key' => 'novelties.default-subtraction-balance-novelty-type',
                 'name' => 'Default subtraction balance novelty type',
                 'description' => 'Default novelty type for subtraction balance',
                 'data_type' => 'int',
-                'value' => NoveltyType::where('code', 'B-')->first()->id,
+                'value' => (string) NoveltyType::where('code', 'B-')->first()->id,
             ],
         ];
     }
