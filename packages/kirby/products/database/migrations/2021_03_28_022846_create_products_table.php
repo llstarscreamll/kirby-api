59acb19970f78c4f55c1791d8763e7322e1685af
Johan Alvarez <llstarscreamll@hotmail.com>
2021-03-27 22:52:48 -0500

    feat(products): package created

---
+++ packages/kirby/products/database/migrations/2021_03_28_022846_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('internal_code')->unique();
+            $table->string('customer_code')->unique();
+            $table->string('name');
+            $table->string('diameter_in_mm')->comment('diameter in millimeters');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ packages/kirby/products/src/ProductsServiceProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace kirby\Products;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProductsServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'kirby');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'kirby');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/products.php', 'products');
+
+        // Register the service the package provides.
+        $this->app->singleton('products', function ($app) {
+            return new Products();
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['products'];
+    }
+
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole(): void
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+            __DIR__.'/../config/products.php' => config_path('products.php'),
+        ], 'products.config');
+
+        // Publishing the views.
+        /*$this->publishes([
+        __DIR__.'/../resources/views' => base_path('resources/views/vendor/kirby'),
+        ], 'products.views');*/
+
+        // Publishing assets.
+        /*$this->publishes([
+        __DIR__.'/../resources/assets' => public_path('vendor/kirby'),
+        ], 'products.views');*/
+
+        // Publishing the translation files.
+        /*$this->publishes([
+        __DIR__.'/../resources/lang' => resource_path('lang/vendor/kirby'),
+        ], 'products.views');*/
+
+        // Registering package commands.
+        // $this->commands([]);
+    }
+}
