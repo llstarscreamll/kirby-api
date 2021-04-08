7fd4f522a19b3f7e79929f5329a58b9285ad0a2e
Johan Alvarez <llstarscreamll@hotmail.com>
2021-04-07 21:26:47 -0500

    feat(products): endpoint to search for products

---
+++ packages/kirby/products/database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+use Faker\Generator as Faker;
+use Kirby\Products\Models\Product;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+ */
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'internal_code' => $faker->unique()->word(),
+        'customer_code' => $faker->unique()->word(),
+        'name' => $faker->numerify('PROD-##'),
+        'diameter_in_mm' => $faker->numberBetween(1, 100),
+    ];
+});
--- packages/kirby/products/src/ProductsServiceProvider.php
+++ packages/kirby/products/src/ProductsServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace kirby\Products;
 
+use Illuminate\Database\Eloquent\Factory as EloquentFactory;
 use Illuminate\Support\ServiceProvider;
 
 class ProductsServiceProvider extends ServiceProvider
@@ -16,11 +17,15 @@ class ProductsServiceProvider extends ServiceProvider
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'kirby');
         // $this->loadViewsFrom(__DIR__.'/../resources/views', 'kirby');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadRoutesFrom(__DIR__.'/UI/API/V1/routes.php');
 
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
             $this->bootForConsole();
+        }
+
+        if ($this->app->runningUnitTests()) {
+            $this->app->make(EloquentFactory::class)->load(__DIR__.'/../database/factories');
         }
     }
 
