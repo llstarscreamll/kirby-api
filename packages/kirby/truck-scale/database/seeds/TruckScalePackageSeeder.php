7e2b07f1e3a36cb9f8c49e93c14298850c23c899
Johan Alvarez <llstarscreamll@hotmail.com>
2023-05-15 23:00:00 -0500

    feat(truck-scale): add seeder for module permissions

---
+++ packages/kirby/truck-scale/database/seeds/TruckScalePackageSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Kirby\Authorization\Models\Permission;
+
+class TruckScalePackageSeeder extends Seeder
+{
+	private $permissions = [
+        ['name' => 'truck-scale.search'],
+        ['name' => 'truck-scale.create'],
+        ['name' => 'truck-scale.update'],
+    ];
+
+	public function run()
+    {
+        collect($this->permissions)->map(function ($permission) {
+            return Permission::updateOrCreate($permission);
+        });
+    }	
+}
