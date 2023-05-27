3c2bac35815f78f3c6261056cac4036fe2b9dad7
Johan Alvarez <llstarscreamll@hotmail.com>
2023-05-26 22:11:14 -0500

    feat(truckScale): add auth middleware to api routes

--- packages/kirby/truck-scale/src/UI/API/V1/routes.php
+++ packages/kirby/truck-scale/src/UI/API/V1/routes.php
@@ -6,7 +6,7 @@ use Kirby\TruckScale\UI\API\V1\Controllers\DriversController;
 use Kirby\TruckScale\UI\API\V1\Controllers\VehiclesController;
 use Kirby\TruckScale\UI\API\V1\Controllers\WeighingsController;
 
-Route::group(['prefix' => 'api/1.0'], function (Router $route) {
+Route::group(['prefix' => 'api/1.0', 'middleware' => 'auth:api'], function (Router $route) {
     $route->apiResource('vehicles', VehiclesController::class)->only(['index']);
     $route->apiResource('drivers', DriversController::class)->only(['index']);
     $route->apiResource('weighings', WeighingsController::class)->only(['store']);
